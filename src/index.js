d925505e30ba6e9c7c8cc1e7350b403be13b5b42
Chubbard022 <chubbard022@gmail.com>
2020-08-22 07:39:26 -0700

    condensing src to one file, getting rid of index.js

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const server = require("./server.js")
-const port = 3500;
-
-server.listen(port,()=>{
-    console.log(`Server listening on ${port}`)
-})
--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
+//setting up server
 const server = express();
-
 server.use(express.json())
+
+//server listening on port 3500
+const port = 3500;
+server.listen(port,()=>{
+    console.log(`Server listening on ${port}`)
+})
 
 module.exports = server;     

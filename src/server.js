85f44f28a9f33de3dbb85e8e39264c207b5d0efa
Chubbard022 <chubbard022@gmail.com>
2020-08-24 09:43:08 -0700

    adding priority route

--- src/server.js
+++ src/server.js
@@ -2,13 +2,15 @@ const express = require("express")
 
 const listRoutes = require("../routes/listRoutes.js")
 const itemRoutes = require("../routes/itemRoutes")
+const priorityRoutes = require("../routes/priorityRoutes")
 
 //setting up server
 const server = express();
 server.use(express.json())
 
-server.use("/api/list",listRoutes)
-server.use("/api/item",itemRoutes)
+server.use("/api/list",listRoutes);
+server.use("/api/item",itemRoutes);
+server.use("/api/priority",priorityRoutes);
 
 server.use("/",(req,res)=>{
     res.status(200).json("server working")
